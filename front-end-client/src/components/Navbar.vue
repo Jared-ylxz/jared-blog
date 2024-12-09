dba63ccfdbd7a74848a8f3d2706155df05481097
Jared <jaredzheng@vectorbuilder.net>
2024-12-10 00:06:10 +0800

    优化导航栏样式

--- front-end-client/src/components/Navbar.vue
+++ front-end-client/src/components/Navbar.vue
@@ -1,6 +1,10 @@
 <template>
     <nav class="navbar">
-      <router-link to="/">首页</router-link>
+      <router-link class="nav-link" to="/">首页</router-link>
+      <RouterLink class="nav-link" to="/blog-create">新建</RouterLink>
+      <RouterLink class="nav-link" to="/">友链</RouterLink>
+      <RouterLink class="nav-link" to="/">登录</RouterLink>
+      <RouterLink class="nav-link" to="/">注册</RouterLink>
     </nav>
 </template>
   
@@ -13,6 +17,22 @@
     backdrop-filter: blur(4px); /* 背景模糊效果 */
     z-index: 1000; /* 确保导航栏在其他内容之上 */
   }
+
+  .nav-link{
+    font-weight: 600;
+    text-decoration: none;
+    color: #ffffffda;
+    padding: 5px 15px;
+    border-radius: 5px;
+    transition: all 0.2s ease-in-out;
+    margin-right: 1rem;
+    &:hover{
+      background: #ffffff48;
+      font-weight: 900;
+      color: #ffffff;
+    }
+  }
+
   .navbar a {
     color: white;
     text-decoration: none;
